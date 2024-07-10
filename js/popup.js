400eef601f58288f041f6eadf6d3678e3c0d4fd1
mounirzx <102691929+mounirzx@users.noreply.github.com>
2024-07-10 22:31:20 +0200

    initial commit

---
+++ js/popup.js
@@ -0,0 +1,208 @@
+let entries = [];
+function NewEntry( name ) {
+    let entry = {
+        name:      name,
+        wilaya:    '0',
+        nin:       '0',
+        nss:       '0',
+        telephone: '0',
+
+        tab_id:     -1,
+        running:    false,
+        success:    false,
+    }
+
+    entries.push( entry );
+    return entry;
+}
+
+// read/parse entries file
+document.querySelector( "input[type=file]" ).onchange = async (e) => {
+    // read
+    const file = e.target.files.item( 0 );
+    const text = await file.text();
+
+    // parse
+    entries.length = 0; // reset
+    const lines = text.split( '\n' );
+    let current_entry;
+    for( line of lines )
+    {
+        // ignore empty lines
+        if( line.trim().length === 0  )
+            continue;
+
+        let splits = line.split( ':' );
+        let title  = splits[0].trim().toUpperCase();       
+        let data   = splits[1].trim().toUpperCase();       
+        switch( title ) {
+            case "NOM":
+                current_entry = NewEntry( data );
+                break;
+            case "WIL":
+                current_entry.wilaya = data;
+                break;
+            case "NIN":
+                current_entry.nin = data;
+                break;
+            case "NSS":
+                current_entry.nss = data;
+                break;
+            case "TEL":
+                current_entry.tel = data;
+                break;
+
+            default:
+                console.error( "Line title unknown : " + title )
+        }
+    }
+
+    // push to DOM
+    UpdateUI();
+    PresistEntries();
+}
+
+// when popup is re-oppened
+chrome.storage.session.get( ["entries"], (result) => {
+    if( result.entries === undefined )
+        return;
+
+    entries = result.entries;
+    UpdateUI();
+}) 
+
+const start_all_btn = document.getElementById( "start_all_btn" );
+const stop_all_btn  = document.getElementById( "stop_all_btn" );
+
+start_all_btn.onclick = (e) => {
+    for( entry of entries )
+        if( !entry.running )
+            StartTask( entry )
+}
+stop_all_btn.onclick = (e) => {
+    for( entry of entries )
+        if( entry.running )
+            StopTask( entry )
+}
+
+function UpdateUI() {
+    let atleast_one_running = false;
+    let atleast_one_stopped = false;
+
+    const ol = document.getElementsByTagName( "ol" )[0];
+    ol.innerHTML = ""; // clear
+
+    for( const [i, entry] of entries.entries() ) 
+    {
+        /*
+            <li>
+                <span>ZEROUAL Aymene</span>
+                <button>VIEW</button>
+                <button>START</button>
+                <button>STOP</button>
+            </li>
+            <hr>
+        */
+        const view_btn = document.createElement( "button" );
+        view_btn.dataset.trans = "VIEW"
+        view_btn.innerHTML     = TRANSLATE( "VIEW" );
+        view_btn.disabled  = ( entry.tab_id === -1 )? true : false;
+        
+        const span = document.createElement( "span" );
+        span.innerHTML = entry.name;
+        
+        let name_state_class = "running";
+        if( entry.success )
+            name_state_class = "success";
+        else if( !entry.running )
+            name_state_class = "stopped";
+        span.classList.add( name_state_class )
+
+        const start_btn = document.createElement( "button" );
+        start_btn.dataset.trans = "START"
+        start_btn.innerHTML     = TRANSLATE( "START" );
+        start_btn.id        = "start_btn_" + i; 
+        start_btn.disabled  = entry.running;
+        start_btn.classList.add( "start" );
+
+        const stop_btn = document.createElement( "button" );
+        stop_btn.dataset.trans = "STOP"
+        stop_btn.innerHTML     = TRANSLATE( "STOP" );
+        stop_btn.id        = "stop_btn_" + i; 
+        stop_btn.disabled  = !entry.running;
+        stop_btn.classList.add( "stop" );
+
+        const li = document.createElement( "li" );
+        li.appendChild( span );
+        li.appendChild( view_btn );
+        li.appendChild( start_btn );
+        li.appendChild( stop_btn );
+
+        ol.appendChild( li );
+        ol.appendChild( document.createElement( "hr" ) );
+
+        if( entry.running )
+            atleast_one_running = true;
+        else    
+            atleast_one_stopped = true;
+
+        // events
+        view_btn.onclick  = (e) => chrome.tabs.update( entry.tab_id, { selected: true } )
+        start_btn.onclick = (e) => StartTask( entry );
+        stop_btn.onclick  = (e) => StopTask( entry );
+    }
+
+    // stop/start all buttons
+    if( atleast_one_running ) 
+        stop_all_btn.disabled = false;
+    else
+        stop_all_btn.disabled = true;
+
+    if( atleast_one_stopped ) 
+        start_all_btn.disabled = false;
+    else
+        start_all_btn.disabled = true;
+}
+ 
+const url = "https://www.aadl3inscription2024.dz/";
+//const url = "http://localhost:8000/";
+function StartTask( entry ) {
+    entry.running = true;
+    entry.success = false;
+
+    // tab already open just reload it
+    if( entry.tab_id !== -1 ) {
+        chrome.tabs.reload( entry.tab_id );
+
+        PresistEntries();
+        UpdateUI();
+    } else {
+        chrome.tabs.create( {
+            active: false,
+            url:    url
+        } ).then( (tab) => {
+            entry.tab_id  = tab.id;
+
+            PresistEntries();
+            UpdateUI();
+        } );
+    }
+}
+
+function StopTask( entry ) {
+    entry.running = false;
+    PresistEntries()
+    UpdateUI();
+}
+
+function PresistEntries() {
+    chrome.storage.session.set( {
+        entries: entries
+    } )    
+}
+
+// Github icon href 
+document.getElementsByTagName( "svg" )[0].onclick = (e) => {
+    console.log("dd")
+    chrome.tabs.create( { url: "https://www.github.com/zraymene" } )
+}
---
+++ js/service_worker.js
@@ -0,0 +1,151 @@
+const url = "https://www.aadl3inscription2024.dz/";
+//const url = "http://localhost:8000/";
+
+// first run
+chrome.runtime.onInstalled.addListener((details) => { 
+    console.log( "Installed..." );
+    PresistEntries( [] );
+})
+
+// listen to network errors and filter them
+chrome.webRequest.onErrorOccurred.addListener( onNetworkError, { urls: [url, url + "    */"] } );
+function onNetworkError( details ) {
+    LoadEntries( (entries) => {
+
+        // is it from the concerned tabs ?
+        let target_entry = GetEntryByTabID( entries, details.tabId );
+        if( target_entry === null || target_entry === undefined ) 
+            return;
+
+        // is the entry still running
+        if( !target_entry.running )
+            return;
+
+        // we care only about page loading
+        if( details.method !== "GET" )
+            return;
+
+        // reload tab
+        chrome.tabs.reload( details.tabId )
+        .then( (e) => console.log( "Reloading tab: " + target_entry.name ) )
+        .catch( (e) => {
+            console.log( "Tried to close: " + target_entry.name + ". Stopping it." );
+
+            target_entry.running = false;
+            target_entry.tab_id = -1;
+            
+            PresistEntries( entries );
+        } );
+    } )
+}
+
+// Page loaded
+chrome.webNavigation.onCompleted.addListener( onSuccess, { urls: [url] } )
+function onSuccess( details ) {
+    LoadEntries( (entries) => {
+
+        // is it from the concerned tabs ?
+        let target_entry = GetEntryByTabID( entries, details.tabId );
+        if( target_entry === null || target_entry === undefined ) 
+            return;
+
+        // stop it from refreshing
+        target_entry.running = false;
+        target_entry.success = true;
+        PresistEntries( entries );
+
+        // notification
+        chrome.notifications.create( "AADL3, " + target_entry.name, { 
+            iconUrl: "images.jpeg", 
+            title:  "AADL3, " + target_entry.name,
+            message: "AADL3, " + target_entry.name,
+            type: "basic"
+        } )
+
+        // set it as active
+        chrome.tabs.update( target_entry.tab_id, { selected: true } )
+
+        console.log( "Tab: " + target_entry.name + " - loaded !" );
+
+        // inject content script 
+        chrome.scripting.executeScript( {
+            target: { tabId: target_entry.tab_id },
+            func:   FillForms,
+            args:   [ target_entry ]
+        })
+    } )
+}
+function GetEntryByTabID( entries, tab_id ) {
+    for( entry of entries )
+        if( entry.tab_id === tab_id )
+            return entry;
+}
+
+// delete closed tabs
+chrome.tabs.onRemoved.addListener( (tabId, _ ) => {
+    LoadEntries( (entries) => {
+        for( entry of entries ) {
+            if( tabId === entry.tab_id ) {
+                entry.running = false;
+                entry.tab_id  = -1;
+
+                PresistEntries( entries );
+            }         
+        }
+    } )
+} )
+
+// --------- Injected JS code ----------
+function FillForms( entry ) {
+    document.title = entry.name;
+
+    // enable back: copy, cut, paste anc context menu
+    /*const body_html_content = structuredClone( document.body.innerHTML );
+    document.body = document.body = document.createElement("body");
+    document.body.innerHTML = body_html_content;*/
+
+    // click on the button
+    const orange_button = document.getElementById( "A14" );
+    orange_button.click();
+    console.log( "Clicked !" );
+    
+    // wait 1 sec
+    setTimeout( ()  => {
+        const select_wilaya   = document.getElementById( "A17" );
+        const input_nin       = document.getElementById( "A22" );
+        const input_nss       = document.getElementById( "A27" );
+        const input_telephone = document.getElementById( "A13" );
+        //const accept_checkbox = document.getElementById( "A91_1" );
+    
+        input_nin.oncontextmenu = null;
+    
+        console.log( input_nin )
+        console.log( input_nss )
+        console.log( input_telephone )
+    
+        // change values
+        select_wilaya.value   = parseInt( entry.wilaya ) + 1;
+        input_nin.value       = entry.nin;
+        input_nss.value       = entry.nss;
+        //input_telephone.value = entry.telephone;
+
+        console.log( "Form filled !" );
+    }, 1000 )
+}
+
+// --------- Storage utility ----------
+function LoadEntries( callback ) {
+    chrome.storage.session.get( ["entries"], 
+        (result) =>  { 
+            let entries = result.entries
+            if( entries === null || entries === undefined ) 
+                return;
+
+            callback( entries );
+    });
+}
+function PresistEntries( entries ) {
+    chrome.storage.session.set( {
+        entries: entries
+    } )    
+}
